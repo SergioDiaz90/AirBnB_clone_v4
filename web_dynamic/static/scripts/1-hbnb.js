ea33884a5a15551fab78066ac3bf687c995df45c
julianramirezch <julianramirezch1@gmail.com>
2020-09-25 22:53:31 +0000

    [add]: Select some Amenities to be comfortable1-hbnb.py, templates/1-hbnb.html, static/scripts/1-hbnb.js

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,9 +1,13 @@
-$(document).ready(function() {
-    const amenites = []
-    $('#amenity.id').click((dict) => {
-        if ($(this).is(':checked')) {
-            amenites.push(dict);
+$(document).ready(function () {
+    const amenities = {}
+    $('input[type="checkbox"]').change(function (elm) {
+        if ($(this).is(":checked")) {
+            amenities[$(this).attr('data-name')] = $(this).attr('data-id');
+            $('.amenities h4').text(Object.keys(amenities));
+        } else{
+            delete amenities[$(this).attr('data-name')];	
         }
-        console.log(amenites);
+	  const dataNames = Object.keys(amenities);
+    $('.amenities h4').text(dataNames.join(', '));
     });
 });
